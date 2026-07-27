89c45b206280aa21b547b0c0e23a3bff9b0042a4
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 01:38:04 +0200

    Restore quality and sources page

---
+++ src/app/(protected)/quality/page.tsx
@@ -0,0 +1,7 @@
+import { LibraryPageClient } from '@/components/library-page-client'
+
+export const metadata = { title: 'Qualité & sources' }
+
+export default function QualityPage() {
+  return <LibraryPageClient view="quality" />
+}
