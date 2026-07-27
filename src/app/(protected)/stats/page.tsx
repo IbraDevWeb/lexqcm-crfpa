5996b5eb465b02a6e454506ef6f6d4210374a5c9
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 01:37:37 +0200

    Restore statistics page

---
+++ src/app/(protected)/stats/page.tsx
@@ -0,0 +1,7 @@
+import { LibraryPageClient } from '@/components/library-page-client'
+
+export const metadata = { title: 'Statistiques' }
+
+export default function StatsPage() {
+  return <LibraryPageClient view="stats" />
+}
