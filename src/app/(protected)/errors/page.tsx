a8e200f22fd38a9b955d209f3629c448cf4d1d0c
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 01:37:24 +0200

    Restore errors page

---
+++ src/app/(protected)/errors/page.tsx
@@ -0,0 +1,7 @@
+import { LibraryPageClient } from '@/components/library-page-client'
+
+export const metadata = { title: 'Mes erreurs' }
+
+export default function ErrorsPage() {
+  return <LibraryPageClient view="errors" />
+}
