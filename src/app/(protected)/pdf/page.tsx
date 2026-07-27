85ec9a6507191a265f53a054f962d48d37844f32
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 01:39:22 +0200

    Restore private PDF library page

---
+++ src/app/(protected)/pdf/page.tsx
@@ -0,0 +1,7 @@
+import { PdfLibraryClient } from '@/components/pdf-library-client'
+
+export const metadata = { title: 'Cours & PDF' }
+
+export default function PdfPage() {
+  return <PdfLibraryClient />
+}
