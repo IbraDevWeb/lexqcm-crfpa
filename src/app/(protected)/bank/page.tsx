017f5d669474295c191e75fd2bf75d75da29d98e
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 01:37:44 +0200

    Restore QCM bank page

---
+++ src/app/(protected)/bank/page.tsx
@@ -0,0 +1,7 @@
+import { LibraryPageClient } from '@/components/library-page-client'
+
+export const metadata = { title: 'Banque QCM' }
+
+export default function BankPage() {
+  return <LibraryPageClient view="bank" />
+}
