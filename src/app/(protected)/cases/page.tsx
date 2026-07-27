8eaa4d36d043d903632223deb6695242b5f4f8d1
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-28 00:06:27 +0200

    Add dossiers progressifs page

---
+++ src/app/(protected)/cases/page.tsx
@@ -0,0 +1,7 @@
+import { CaseClient } from '@/components/case-client'
+
+export const metadata = { title: 'Dossiers progressifs' }
+
+export default function CasesPage() {
+  return <CaseClient />
+}
